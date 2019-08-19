417aeafdd4547c4e3229da19de9514a1b03509b5
Ruslan Ka <octopused@gmail.com>
2019-08-19 12:11:50 +0500

    Initial commit

---
+++ MyPod/Classes/Helpers.swift
@@ -0,0 +1,15 @@
+//
+//  Helpers.swift
+//  MyPod
+//
+//  Created by RuslanKa on 19/08/2019.
+//  Copyright © 2019 RuslanKa. All rights reserved.
+//
+
+import Foundation
+
+public class MathFuncs {
+    public static func power(n1: Double, n2: Double) -> Double {
+        return n1 * n2
+    }
+}
---
+++ Pods/Target Support Files/Pods-MyPod/Pods-MyPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyPod : NSObject
+@end
+@implementation PodsDummy_Pods_MyPod
+@end
