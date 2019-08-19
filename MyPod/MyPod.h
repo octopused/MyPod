94f755aef961a2874f31dd47489dc5eca4df4372
Ruslan Ka <octopused@gmail.com>
2019-08-19 11:54:45 +0500

    Initial Commit

---
+++ MyPod/MyPod.h
@@ -0,0 +1,19 @@
+//
+//  MyPod.h
+//  MyPod
+//
+//  Created by RuslanKa on 19/08/2019.
+//  Copyright © 2019 RuslanKa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyPod.
+FOUNDATION_EXPORT double MyPodVersionNumber;
+
+//! Project version string for MyPod.
+FOUNDATION_EXPORT const unsigned char MyPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPod/PublicHeader.h>
+
+
